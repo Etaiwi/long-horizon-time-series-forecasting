15685b6808045796e428c064cda84567159aca55
Guyinbarr <140887654+Guyinbarr@users.noreply.github.com>
2026-08-06 16:32:10 +0300

    Add files via upload

--- src/ts_project/__init__.py
+++ src/ts_project/__init__.py
@@ -1,3 +1,25 @@
-"""Time-series paper reconstruction project."""
+"""Forecasting model definitions used by the reconstruction notebooks."""
 
-__version__ = "0.1.0"
+from ts_project.models.dlinear import DLinear, MovingAverage, SeriesDecomposition
+from ts_project.models.patchtst import PatchTST
+from ts_project.models.slice_aware import (
+    GatedSliceAwareDLinear,
+    PredictedStatistics,
+    RevINDLinear,
+    SliceAwareDLinear,
+    SliceStatisticsHead,
+    slice_statistics_loss,
+)
+
+__all__ = [
+    "DLinear",
+    "GatedSliceAwareDLinear",
+    "MovingAverage",
+    "PatchTST",
+    "PredictedStatistics",
+    "RevINDLinear",
+    "SeriesDecomposition",
+    "SliceAwareDLinear",
+    "SliceStatisticsHead",
+    "slice_statistics_loss",
+]
